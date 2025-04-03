0ad653a0a0ce0b02411d1bcfc6e823d2f1f6a311
defer logic <defer_logic@proton.me>
2025-04-03 21:27:31 +0300

    1.0.4

--- empctl.rb
+++ empctl.rb
@@ -1,24 +1,24 @@
 class Empctl < Formula
     desc "A CLI tool from Emp Cloud"
     homepage "https://empyrealsdk.com"
-    version "1.0.3"
+    version "1.0.4"
 
     if OS.mac? && Hardware::CPU.intel?
       # macOS (Intel)
-      url "https://empyrealsdk-binaries.s3.amazonaws.com/empctl-v1.0.3-darwin-amd64.tar.gz"
-      sha256 "059e8bc337ac3d82d4bffeb437ec13eab395da8362cbf3f7cd5e420849b1b8d7"
+      url "https://empyrealsdk-binaries.s3.amazonaws.com/empctl-v1.0.4-darwin-amd64.tar.gz"
+      sha256 "ee0fa8bf30dcb8577801fdb3f42989b776c20728e0aae2bd37cbd59eebf22f88"
     elsif OS.mac? && Hardware::CPU.arm?
       # macOS (Apple Silicon)
-      url "https://empyrealsdk-binaries.s3.amazonaws.com/empctl-v1.0.3-darwin-arm64.tar.gz"
-      sha256 "bcd5a5b0c3d757e340dfe201068781b62d8e58b8cf2d86efea1bb336bf303cb6"
+      url "https://empyrealsdk-binaries.s3.amazonaws.com/empctl-v1.0.4-darwin-arm64.tar.gz"
+      sha256 "248f3839b87db6bbbafe727f45b3945cd91e23dbe77cacb457f0beb2ae82d697"
     elsif OS.linux? && Hardware::CPU.intel?
       # Linux (Intel/AMD 64-bit)
-      url "https://empyrealsdk-binaries.s3.amazonaws.com/empctl-v1.0.3-linux-amd64.tar.gz"
-      sha256 "420ee4686050cc6ca21323c51b7d15f68d977cb51e97ee9522a8348eef9fbe71"
+      url "https://empyrealsdk-binaries.s3.amazonaws.com/empctl-v1.0.4-linux-amd64.tar.gz"
+      sha256 "f77fe400bced628c2b66cd53f9542fa421875e5355092be5e8bcc505bda61f3d"
     elsif OS.linux? && Hardware::CPU.arm?
       # Linux (ARM 64, e.g. Graviton)
-      url "https://empyrealsdk-binaries.s3.amazonaws.com/empctl-v1.0.3-linux-arm64.tar.gz"
-      sha256 "b963c50f8cd207d059d895d790fe6690c21fe57c3a796cffd321d7adeb1f23bd"
+      url "https://empyrealsdk-binaries.s3.amazonaws.com/empctl-v1.0.4-linux-arm64.tar.gz"
+      sha256 "a9e0eae8369e52b8f3a978d31001bb1ced3b9a1dc36b7ae601c9714fb567d23c"
     else
       odie "Binary not available for this platform."
     end
