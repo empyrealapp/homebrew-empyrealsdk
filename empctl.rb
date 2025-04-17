000c0a8b214a85c09046a4348c4ecbc581d2efd3
defer logic <defer_logic@proton.me>
2025-04-17 16:42:10 +0300

    v1.0.7

--- empctl.rb
+++ empctl.rb
@@ -1,24 +1,24 @@
 class Empctl < Formula
     desc "A CLI tool from Emp Cloud"
     homepage "https://empyrealsdk.com"
-    version "1.0.6"
+    version "1.0.7"
 
     if OS.mac? && Hardware::CPU.intel?
       # macOS (Intel)
-      url "https://empyrealsdk-binaries.s3.amazonaws.com/empctl-v1.0.6-darwin-amd64.tar.gz"
-      sha256 "9e58641e32db2ecf9f242b6c05ef96d84463eb210c019d253ba922ae64e0b78d"
+      url "https://empyrealsdk-binaries.s3.amazonaws.com/empctl-v1.0.7-darwin-amd64.tar.gz"
+      sha256 "93baad7fee326a2c01923719b34414576c0a05088d5f03ab691ad6f4a9493b03"
     elsif OS.mac? && Hardware::CPU.arm?
       # macOS (Apple Silicon)
-      url "https://empyrealsdk-binaries.s3.amazonaws.com/empctl-v1.0.6-darwin-arm64.tar.gz"
-      sha256 "15d396bdabd8e1f4a34f3ff43d9767ce9ad86a5b52748d496ce9f74c4746a6a0"
+      url "https://empyrealsdk-binaries.s3.amazonaws.com/empctl-v1.0.7-darwin-arm64.tar.gz"
+      sha256 "80a272b94f1561f48d66e2ad100ac921fd507177b0268259db74e93f1e570090"
     elsif OS.linux? && Hardware::CPU.intel?
       # Linux (Intel/AMD 64-bit)
-      url "https://empyrealsdk-binaries.s3.amazonaws.com/empctl-v1.0.6-linux-amd64.tar.gz"
-      sha256 "5fa7a535401ed5e35f2df30dc7a34bbd41b186349b2a80ac117de6a55505758e"
+      url "https://empyrealsdk-binaries.s3.amazonaws.com/empctl-v1.0.7-linux-amd64.tar.gz"
+      sha256 "4651a0dd854fac23e0c16b6295232e5e3ac5b0e617286807ec159a1349aec221"
     elsif OS.linux? && Hardware::CPU.arm?
       # Linux (ARM 64, e.g. Graviton)
-      url "https://empyrealsdk-binaries.s3.amazonaws.com/empctl-v1.0.6-linux-arm64.tar.gz"
-      sha256 "db29a56fbfb0719b9521a867edab5b6be47947b5e1e93ff0ba802c2f585f2b12"
+      url "https://empyrealsdk-binaries.s3.amazonaws.com/empctl-v1.0.7-linux-arm64.tar.gz"
+      sha256 "1b1145033601b22f6103397c9d8e35a9fc20c8dc1835626f4b3d00f069168ff3"
     else
       odie "Binary not available for this platform."
     end
