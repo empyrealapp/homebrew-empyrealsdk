a5fbf6e771530dd66edd4ac500dd2fbd9026e00b
defer logic <defer_logic@proton.me>
2025-05-13 09:13:48 +0200

    bump homebrew

--- empctl.rb
+++ empctl.rb
@@ -1,24 +1,24 @@
 class Empctl < Formula
     desc "A CLI tool from Emp Cloud"
     homepage "https://empyrealsdk.com"
-    version "1.0.9"
+    version "1.0.10"
 
     if OS.mac? && Hardware::CPU.intel?
       # macOS (Intel)
-      url "https://empyrealsdk-binaries.s3.amazonaws.com/empctl-v1.0.8-darwin-amd64.tar.gz"
-      sha256 "94a41f6c1ce3e531ecf523c5bc707a9f6205a4662bf8153a613788dd04bb9950"
+      url "https://empyrealsdk-binaries.s3.amazonaws.com/empctl-v1.0.10-darwin-amd64.tar.gz"
+      sha256 "8a7ae1f8b5065ea079f56b6ef109834bbaf484dcb138e00c4457f578681b2b01"
     elsif OS.mac? && Hardware::CPU.arm?
       # macOS (Apple Silicon)
-      url "https://empyrealsdk-binaries.s3.amazonaws.com/empctl-v1.0.8-darwin-arm64.tar.gz"
-      sha256 "c0e5385ebc7214968efa062a5fc138fdeec9ddc18ad1c1495ee94f05a9145569"
+      url "https://empyrealsdk-binaries.s3.amazonaws.com/empctl-v1.0.10-darwin-arm64.tar.gz"
+      sha256 "9cd2ec6798fc0b09cb8a654e7305c4240ea0df01962e4ea58dc07608e80fe1d2"
     elsif OS.linux? && Hardware::CPU.intel?
       # Linux (Intel/AMD 64-bit)
-      url "https://empyrealsdk-binaries.s3.amazonaws.com/empctl-v1.0.8-linux-amd64.tar.gz"
-      sha256 "e16ac1847f501c91e0302f7d25e27ebd5aa935b4d252c3b8e337a86eb671dab5"
+      url "https://empyrealsdk-binaries.s3.amazonaws.com/empctl-v1.0.10-linux-amd64.tar.gz"
+      sha256 "b335b83c5097d83b3fee0a50af51be1515fa617d5cc88e8e949c2a6ef57f405a"
     elsif OS.linux? && Hardware::CPU.arm?
       # Linux (ARM 64, e.g. Graviton)
-      url "https://empyrealsdk-binaries.s3.amazonaws.com/empctl-v1.0.8-linux-arm64.tar.gz"
-      sha256 "a39be874450d2056762d93801202de4fe036b97af6a5b2ab48fa02b23ece5519"
+      url "https://empyrealsdk-binaries.s3.amazonaws.com/empctl-v1.0.10-linux-arm64.tar.gz"
+      sha256 "ac14c723b807f78f483939ba7cf4e1566343a671eb4639826ac2782b68daf56f"
     else
       odie "Binary not available for this platform."
     end
